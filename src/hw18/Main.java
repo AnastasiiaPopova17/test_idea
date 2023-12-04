f0f986e6a6c8d324db6f389e3dd64ffe17671f4a
AIT TR Student <17popovaas@gmail.com>
2023-12-04 22:54:59 +0100

    added task 2

--- src/hw18/Main.java
+++ src/hw18/Main.java
@@ -1,5 +1,7 @@
 package hw18;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         Student student = new Student("Jack", 25);
@@ -9,5 +11,27 @@ public class Main {
         System.out.println("Student " + student.name + " is "+ student.age + " years old");
         System.out.println("Student " + student1.name + " is "+ student1.age + " years old");
         System.out.println("Student " + student2.name + " is "+ student2.age + " years old");
+
+        System.out.println("*******************");
+
+        Rectangle rectangle1 = new Rectangle(4,5);
+        Rectangle rectangle2 = new Rectangle(8,6);
+        Rectangle rectangle3 = new Rectangle(24,13);
+
+        System.out.println("Rectangel 1 has "+ rectangle1.length + " length and "+ rectangle1.width + " width" );
+        rectangle1.calculatePerimetr();
+        rectangle1.calculateSquare();
+
+        System.out.println();
+        System.out.println("Rectangel 2 has "+ rectangle2.length + " length and "+ rectangle2.width + " width" );
+        rectangle2.calculatePerimetr();
+        rectangle2.calculateSquare();
+
+        System.out.println();
+        System.out.println("Rectangel 3 has "+ rectangle3.length + " length and "+ rectangle3.width + " width" );
+        rectangle3.calculatePerimetr();
+        rectangle3.calculateSquare();
+
+
     }
 }
