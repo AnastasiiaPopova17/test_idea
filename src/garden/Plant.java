3a6b973ba9bcc14d074cef1a74155161b784729d
AIT TR Student <17popovaas@gmail.com>
2023-12-20 09:27:05 +0100

    couldn't do it...

--- src/garden/Plant.java
+++ src/garden/Plant.java
@@ -10,8 +10,9 @@ public abstract class Plant {
         this.height = height;
         this.age = age;
         }
-    public void doSpring(){
+    public int doSpring(){
         System.out.println("The " + name + " has grown, " + height);
+        return height = height*2;
     }
     public void doWinter(){
         System.out.println("The " + name +  " hasn't grown in winter, " + height);
